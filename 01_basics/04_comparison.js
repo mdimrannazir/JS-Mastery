5bedbc00eedc320b654655edec8189a2c488fb6d
Md Imran Nazir <39686923+mdimrannazir@users.noreply.github.com>
2023-12-19 15:59:13 +0000

    comparsion operators

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,18 @@
+// console.log(2>1);
+// console.log(2>=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+console.log(null>0);
+console.log(null<0);
+console.log(null==0);
+console.log(null>=0);
+console.log(null<=0);
+
+// === means data type & value check
+
+console.log("-----------")
+console.log("2" === 2)
+console.log(2 === 2)
+console.log(5 === 2)
