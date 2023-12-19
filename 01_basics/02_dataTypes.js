e10ecc2dec010c8b8e1bf9cb4dfbb53d1a372b69
Md Imran Nazir <39686923+mdimrannazir@users.noreply.github.com>
2023-12-19 14:15:31 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict"; //Treat all js code as newer version
+
+// alert("Imran") //can not run it using nodeJS, can run in browser
+
+console.log(3+3) 
+console.log("Imran")
+
+let name = "Imran" //String 
+let age = 20 //Integer => numbers
+let isLoggedIn = false //Bolean => true or false
+let state = null // null => Empty value or no value
+// Symbol => unique
+// object
+
+console.log(typeof null) //Object
+console.log(typeof undefined) //undefined
