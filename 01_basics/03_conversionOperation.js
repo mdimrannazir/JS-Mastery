5ce9d60cb52da68695d9986389870e7499ba9d7c
Md Imran Nazir <39686923+mdimrannazir@users.noreply.github.com>
2023-12-19 15:37:04 +0000

    More Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -49,3 +49,44 @@ let someNumber = 44
 let stringNumber = String(someNumber)
 console.log(typeof(stringNumber))
 console.log(stringNumber)
+
+// ********************* Operations **********************
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+let str1 = "Imran"
+let str2 = " Nazir"
+str3 = str1 + str2 
+console.log(str3)
+
+console.log("1" + 2);      //12
+console.log(1 + "2");      //12
+console.log("1" + 2 + 2);  //122
+console.log(1 + 2 + "2");  //32
+
+let gameCounter = 100
+gameCounter++ //Postfix operator
+// ++gameCounter //Prefix  operator
+console.log(gameCounter)
+
+let x = 3;
+const y = x++;
+// x is 4; y is 3
+console.table([x,y])
+
+let x2 = 3n;
+const y2 = x2++;
+// x2 is 4n; y2 is 3n
+console.table([x2,y2])
+
+// let x = 3;
+// const y = ++x;
+// // x is 4; y is 4
+// console.table([x,y])
+
+// let x2 = 3n;
+// const y2 = ++x2;
+// // x2 is 4n; y2 is 4n
+// console.table([x2,y2])
