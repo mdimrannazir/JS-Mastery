f5c6a008599e499efccc5a6d3681de9d2d24620b
Md Imran Nazir <39686923+mdimrannazir@users.noreply.github.com>
2023-12-20 11:21:31 +0000

    dataType Summary

---
+++ 01_basics/05_dataTypesSummary.js
@@ -0,0 +1,33 @@
+// Primitive data type (Call by value)
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.33
+// const score = "result"
+// JS is dynamic bcz we don't need define their datatype
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherID = Symbol('123')
+
+console.log("Symbol vs Sybol(even they've same value, they are not equal):",id === anotherID);
+
+// *******Non Primitive data type or Reference*********
+// Array, Objects, Functions
+
+const heroes = ["apple", "banana", "orange"]
+
+let myObject = {
+    name: "Imran",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World!")
+}
+
+console.log("heroes: ",typeof(heroes))
+console.log("myObject: ",typeof(myObject))
+console.log("myFunction: ",typeof(myFunction))
