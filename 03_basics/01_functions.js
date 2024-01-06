e88fa0d85498a3c46105b4c35208916db6a2ba87
Md Imran Nazir <39686923+mdimrannazir@users.noreply.github.com>
2024-01-06 12:41:26 +0000

    functions parameter handling

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -27,7 +27,7 @@ function addTwoNumbers(num1, num2){
 
 // const result = addTwoNum(2,2)
 
-// console.log("Result: ", result);
+// console.log("Result: ", result); //undefined bcz not returned
 
 function addTwoNum(n1,n2){
     // let res = n1 + n2
@@ -42,7 +42,7 @@ function addTwoNum(n1,n2){
 
 // More ways to define parameters
 
-function loginUserMessage(username = "sam"){
+function loginUserMessage(username = "sam"){ //default parameter sam, when empty function calll sam will be returned
     if (username === undefined) {
         console.log("Please enter username");
         return
@@ -51,4 +51,44 @@ function loginUserMessage(username = "sam"){
 }
 
 // console.log(loginUserMessage("panda"));
-console.log(loginUserMessage()); //undefined when passed empty, next I set default sam, so when empty returns sam
+// console.log(loginUserMessage()); //undefined when passed empty, next I set default sam, so when empty returns sam
+
+
+// Situation: when I don't know how much parameters can come, we use rest opertaor ...
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200,400,300,500,600));
+
+
+// Objects
+
+const user = {
+    username: "Imran",
+    price: 99
+}
+
+function handleObject(anyObject){
+    console.log(`${anyObject.username} course price is ${anyObject.price}`);
+}
+
+// handleObject(user)
+// Direct pass object when function calling
+handleObject({
+    username: "sam",
+    price: 100
+})
+
+
+//Array
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(anyArray){
+    return anyArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([100,200,300]));
