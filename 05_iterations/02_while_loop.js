583f58dc0458fde7142a50dc052382da0b321020
Md Imran Nazir <39686923+mdimrannazir@users.noreply.github.com>
2024-01-08 10:48:46 +0000

    while loops

---
+++ 05_iterations/02_while_loop.js
@@ -0,0 +1,23 @@
+// let index = 0
+// while(index <= 10){
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+// Arry in while loop
+
+// let myArray = ["flash", "batman", "superman"]
+// let arr = 0
+// while (arr < myArray.length){
+//     console.log(`Value of ${arr} is ${myArray[arr]}`);
+//     arr++
+// }
+
+// Do while loop
+
+let score = 11
+
+do{
+    console.log(`Score is ${score}`);
+    score++
+} while(score <= 10)
